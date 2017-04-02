b3ffa21dcc5cfd324dde4d60fc0dd32b5b2ebc4e
Joseph Thomas <josephdt12@gmail.com>
2017-04-02 16:24:38 -0700

    Project skeleton pages built

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,11 +1,27 @@
-$("#resume").click(function() {
-  $("#projects").removeClass("active");
+$(document).ready(function() {
   $("#resume").addClass("active");
   $("#view").load("resume/resume.html");
 });
 
+$("#home").click(function() {
+  setNavActive("#resume");
+  $("#view").load("resume/resume.html");
+});
+
+$("#resume").click(function() {
+  setNavActive("#resume");
+  $("#view").load("resume/resume.html");
+});
+
 $("#projects").click(function() {
-  $("#resume").removeClass("active");
-  $("#projects").addClass("active");
+  setNavActive("#projects");
   $("#view").load("projects/projects.html");
 });
+
+// Set the id passed in as active, resetting all others
+function setNavActive(id) {
+  $("#home").removeClass("active");
+  $("#resume").removeClass("active");
+  $("#projects").removeClass("active");
+  $(id).addClass("active");
+}
---
+++ projects/projects.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#view-project").load("projects/cpp-quads/cpp-quads.html");
+});
+
+$("#reddit-wallpapers").click(function() {
+  $("#view-project").load("projects/reddit-wallpapers/reddit-wallpapers.html");
+});
+
+$("#cpp-quads").click(function() {
+  $("#view-project").load("projects/cpp-quads/cpp-quads.html");
+});

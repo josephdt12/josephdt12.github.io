1037f21cd67c86017a699dceb5d0ceba4d910e1d
Joseph Thomas <josephdt12@gmail.com>
2017-04-02 05:14:33 -0700

    Redesign - resume almost done

--- app.js
+++
@@ -1,6 +0,0 @@
-angular.module('websiteApp', ['ngRoute'])
-.controller('NavController', ['$location', function($location) {
-  this.isActive = function(viewLocation) {
-    return viewLocation === $location.path();
-  };
-}]);
---
+++ navbar/navbar.js
@@ -0,0 +1,7 @@
+$("#resume").click(function() {
+  $("#view").load("resume/resume.html");
+});
+
+$("#projects").click(function() {
+  $("#view").load("projects/projects.html");
+});

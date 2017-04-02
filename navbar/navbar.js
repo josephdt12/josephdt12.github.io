147771017fdcfbace6f8429fa66197f697a725d9
Joseph Thomas <josephdt12@gmail.com>
2017-04-02 06:08:11 -0700

    Active to navbar, further refine resume

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,7 +1,11 @@
 $("#resume").click(function() {
+  $("#projects").removeClass("active");
+  $("#resume").addClass("active");
   $("#view").load("resume/resume.html");
 });
 
 $("#projects").click(function() {
+  $("#resume").removeClass("active");
+  $("#projects").addClass("active");
   $("#view").load("projects/projects.html");
 });
